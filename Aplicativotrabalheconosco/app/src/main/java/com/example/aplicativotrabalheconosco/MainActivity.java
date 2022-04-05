22eb3f637119174ac83991616b9cf8e6e6f0da75
Marcos-010 <82184743+Marcos-010@users.noreply.github.com>
2022-04-05 19:33:09 -0300

    Add files via upload

---
+++ Aplicativotrabalheconosco/app/src/main/java/com/example/aplicativotrabalheconosco/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.aplicativotrabalheconosco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Private Edittext Nome, Cpf, e-mail;
+    private Button gravar, recuperar;
+    private textview TNome,tCpf,tEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nome = findViewById(R.id.)
+    }
+}
